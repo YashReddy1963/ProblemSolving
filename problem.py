4bcf87e877e86b6bbc52ee5d7562e121e333d201
Yash Reddy <119090589+YashReddy1963@users.noreply.github.com>
2024-12-07 11:03:15 +0530

    Update problem.py

--- problem.py
+++ problem.py
@@ -7,20 +7,18 @@ import random
 
 li = []
 class Student:
-    def __init__(self, data, n):
-
-        for i in range(1,n+1):
-            self.name = data[i][0]
-            self.surname = data[i][1]
-            self.phonenum = data[i][2]
-            self.city = data[i][3]
-            self.year = data[i][4]
-            self.caste = data[i][5]
-            self.result = self.generate_pnr()
-            self.pnr = str(data[i][4]) + data[i][5] + str(self.result)
-            data.setdefault(i,[]).append(self.pnr)
-            li.append(self.pnr)
-            self.admit(data, i)
+    def __init__(self, data):
+        self.name = data[0]
+        self.surname = data[1]
+        self.phonenum = data[2]
+        self.city = data[3]
+        self.year = data[4]
+        self.caste = data[5]
+        self.result = self.generate_pnr()
+        self.pnr = str(data[4]) + data[5] + str(self.result)
+        data.append(self.pnr)
+        li.append(self.pnr)
+        self.admit(data)
             
 
     def generate_pnr(self):
@@ -32,48 +30,41 @@ class Student:
         else:
             self.generate_pnr()
 
-    def admit(self, data, i):
-        print(f"For {data[i][0]} the addmission is confirmed!!")
-        for j in range(0,7):
-            print(data[i][j])
-        self.assign_depart(data, i)
+    def admit(self, data):
+        print(f"For {data[0]} the addmission is confirmed!!")
+        print(f"Details: {data}")
+        self.assign_depart()
     
-    def assign_depart(self, data, i):
+    def assign_depart(self):
         print("1.CSE \n2.ENTC \n3.Mech \n4.Civil \n")
         ch = int(input("Choose your departmen: \n"))
         if ch == 1:
             print("Your department is CSE \n")
-            data.setdefault(i,[]).append("CSE")
         elif ch == 2:
             print("Your department is ENTC \n")
-            data.setdefault(i,[]).append("ENTC")
         elif ch == 3:
             print("Your department is Mech. \n")
-            data.setdefault(i,[]).append("Mech")
         elif ch==4:
             print("Your department is Civil \n")
-            data.setdefault(i,[]).append("Civil")
-        self.assign_courses(data, i)
+        self.assign_courses()
         
 
-    def assign_courses(self, data, i):
+    def assign_courses(self):
         print("1.Maths \n2.Physics \n3.Chemistry \n")
         ch = int(input("Choose your course: \n"))
         if ch==1:
             print("Your subject is Maths\n")
-            data.setdefault(i,[]).append("Maths")
         elif ch == 2:
             print("Your subject is Physics\n")
-            data.setdefault(i,[]).append("Physics")
         elif ch==3:
             print("Your subject is Chemistry \n")
-            data.setdefault(i,[]).append("Chemistry")
 
 
 
-num = int(input("Enter number of students: \n"))
+print("You can enter upto 2 students details: \n")
+totalStud=2
 stud_dict = {}
-for i in range(1,num+1):
+for i in range(1,totalStud+1):
     name=input("Enter the name: \n")
     surname=input("Enter your surname: \n")
     phonenumber=int(input("Enter your phone No.: \n"))
@@ -82,5 +73,6 @@ for i in range(1,num+1):
     caste = input("Enter your caste: \n")
     stud_dict.setdefault(i, [name, surname, phonenumber, city, year, caste])
 
-obj = Student(stud_dict, num)
+obj1 = Student(stud_dict[1])
+obj2 = Student(stud_dict[2])
 print(f"List of generated pnr: {li}")
